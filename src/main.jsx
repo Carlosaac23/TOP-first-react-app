852ee0abba4c7caa0311f89a3eeb374acd6a9b69
Carlos <carlosaac232001@gmail.com>
2025-12-11 17:05:40 -0500

    feat: import new rendering component

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { FavoriteFood } from './FavoriteFood';
 import { Greeting } from './Greeting';
+import { Animals } from './Rendering';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Greeting />
     <FavoriteFood />
+    <Animals />
   </StrictMode>
 );
