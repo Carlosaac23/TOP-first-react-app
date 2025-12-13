b765cc60befd3396c6445fbb62e9f44cb9d62cec
Carlos <carlosaac232001@gmail.com>
2025-12-12 21:31:26 -0500

    chore: use ternary operator to render

--- src/Rendering.jsx
+++ src/Rendering.jsx
@@ -1,12 +1,8 @@
-function ListItem(props) {
-  return <li>{props.animal}</li>;
-}
-
 function List(props) {
   return (
     <ul>
       {props.animals.map(animal => {
-        return <ListItem key={animal} animal={animal} />;
+        return animal.startsWith('L') ? <li key={animal}>{animal}</li> : null;
       })}
     </ul>
   );
