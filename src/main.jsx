ae30d7f30dc0d29b6a8c58701bb98148bee5cb0d
Carlos <carlosaac232001@gmail.com>
2025-12-12 21:46:31 -0500

    chore: import new component

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { FavoriteFood } from './FavoriteFood';
 import { Greeting } from './Greeting';
+import { TodoList } from './Keys';
 import { Animals } from './Rendering';
 import { CarBrands } from './Rendering2';
 import './index.css';
@@ -12,5 +13,6 @@ createRoot(document.getElementById('root')).render(
     <FavoriteFood />
     <Animals />
     <CarBrands />
+    <TodoList />
   </StrictMode>
 );
