b7922fb35c9ab221adf2d174f9328d80045bc091
Carlos <carlosaac232001@gmail.com>
2025-12-13 11:05:32 -0500

    feat: create new component to practice props

---
+++ src/Button.jsx
@@ -0,0 +1,18 @@
+function Button({ text = 'Click Me!', color = 'blue', fontSize = 12 }) {
+  const buttonStyle = {
+    color: color,
+    fontSize: `${fontSize}px`,
+  };
+
+  return <button style={buttonStyle}>{text}</button>;
+}
+
+export default function Buttons() {
+  return (
+    <div>
+      <Button />
+      <Button text="Don't Click Me!" color='red' />
+      <Button fontSize={20} />
+    </div>
+  );
+}
